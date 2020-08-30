d404ae0728929fb100ecb39a9a0745e9f6a5d4ab
Bruno Silva Cruz <brunosc94@hotmail.com>
2020-08-30 10:14:57 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,7 +11,8 @@ public class Program {
 
 
 		Department d = new Department(10, "RH");
-		System.out.println(d);
+		Seller s = new Seller(1, "Bruno", "bruno@hotmail.com", new Date(),2000.00, d);
+		System.out.println(s);
 
 	}
 
