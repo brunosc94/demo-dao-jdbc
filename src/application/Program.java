3afcdb6f3d48d6beab30732386002a553c3b9631
Bruno Silva Cruz <brunosc94@hotmail.com>
2020-08-30 10:36:15 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,11 @@ public class Program {
 
 
 		Department d = new Department(10, "RH");
+		
 		Seller s = new Seller(1, "Bruno", "bruno@hotmail.com", new Date(),2000.00, d);
+		
+		SellerDao selleDao = DaoFactory.createSellerDao();
+		
 		System.out.println(s);
 
 	}
