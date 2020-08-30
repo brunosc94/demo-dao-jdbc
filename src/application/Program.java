0693ac664aa546b8d8cf2ff07160798222fcea4f
Bruno Silva Cruz <brunosc94@hotmail.com>
2020-08-30 10:05:58 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+
+		Department d = new Department(10, "RH");
+		System.out.println(d);
+
+	}
+
+}
